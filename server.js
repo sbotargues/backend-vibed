ea97a48bd51b69b4d849ecac97b098650b54f7ab
Sergi Botargues <sbotargues@gmail.com>
2024-03-03 11:37:20 +0100

    weddings

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use("/api/v1/characters", characterRoutes);
 app.use("/api/v1/business", businessRoutes);
 
 // Iniciar el servidor
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
